ea5dde26784d10be275346719f2a72ed2a217f16
Jesus <jesus.zarate.eng@gmail.com>
2017-11-10 00:41:48 -0700

    Finally got the list to display properly

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,6 +6,7 @@ class Dashboard {
         this.margin = {top: 10, right: 20, bottom: 30, left: 50};
         this.divDashboard = d3.select("#dashboard").classed("fullView", true);
 
+        this.listData = []
         // //fetch the svg bounds
         // this.svgBounds = this.divDashboard.node().getBoundingClientRect();
         // this.svgWidth = this.svgBounds.width - this.margin.left - this.margin.right;
@@ -20,6 +21,7 @@ class Dashboard {
 
     update() {
 
+        let ref = this;
         this.divDashboard.selectAll("ul").remove();
 
         let lu = this.divDashboard.append("ul")
@@ -31,41 +33,81 @@ class Dashboard {
         li.enter()
             .append("li")
             .text(function (d) {
-                return Object.keys(d)[0];
+                //return Object.keys(d)[0];
+                return d[0].key;
             })
             .attr("id", function (d) {
                 return Object.keys(d)[0];
             })
             .on("click", function (d) {
-                let key = Object.keys(d)[0];
+                ref.updateElements(lu, [ref.data]);
+            });
+    }
 
-                let keys = Object.keys(d[key]);
+    updateElements(parentDom, data) {
+        parentDom.selectAll("li").remove();
+        let ref = this;
 
-                let data = [];
-                for (let k in keys){
+        data.forEach(function (k) {
 
-                    data.push(d[key][keys[k]]);
+
+            k.forEach(function (d) {
+                d["children"].forEach(function (k) {
+                    k.forEach(function (d) {
+                        ref.listData.push({"key": d["key"], "children": d["children"], "show": false})
+                    });
+
+                });
+
+            });
+        });
+
+        ref.addElements(parentDom, this.listData)
+    }
+
+    clearList(){
+        this.divDashboard.selectAll("ul").remove();
+    }
+
+    addElements(parentDOM, data) {
+
+        update(this, parentDOM, data);
+
+        function update(ref, parentDom, data) {
+            data.forEach(function (child) {
+                //add li element
+                parentDom.append("li")
+                    .text(child.key)
+                    .on("click", function (d) {
+                        updateShowList(this.innerText, data);
+                        //parentDom.selectAll("li").remove();
+                        ref.clearList();
+                        parentDom = ref.divDashboard.append("ul")
+                            .attr("class", "noBullet");
+                        ref.addElements(parentDom, ref.listData);
+                    });
+                // //if children then make ul
+                if (child.show === true && child.children.length > 0) {
+                    var ul = parentDom.append("ul");
+                    update(ref, ul, child.children[0]);
+                }
+            });
+        }
+
+        function updateShowList(key, data) {
+
+            data.forEach(function (child) {
+
+                if (child.key === key) {
+                    child.show = true;
+                    return;
                 }
 
-                let lu1 = lu.append("ul")
-                    .attr("class", "noBullet");
-
-                let li1 = lu1.selectAll("li")
-                    .data(data);
-
-                li1.enter()
-                    .append("li")
-                    .text(function (d) {
-                        return Object.keys(d)[0];
-                    })
-                    .attr("id", function (d) {
-                        return Object.keys(d)[0];
-                    })
-                    .on("click", function (d) {
-                        alert(Object.keys(d)[0]);
-                    });
+                if (child.children !== undefined) {
+                    updateShowList(key, child.children);
+                }
             });
 
-
+        }
     }
 }
