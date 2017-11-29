f494932dcc5aba034f36b1d6fcad03bdb029dfd9
Jesus <jesus.zarate.eng@gmail.com>
2017-11-29 02:30:36 -0700

    Committing the temp overview.html

--- static/js/overview.js
+++ static/js/overview.js
@@ -71,34 +71,21 @@ class TileChart {
 
             if (d.indexOf(".total") > -1) {
                 let datapoint = this.data[d];
-
-                // point.attr.push({'total': datapoint[datapoint.length - 1][0]});
-                //point.attr['total'] = datapoint[datapoint.length - 1][0];
-                //this.tileData.splice(index, 1, point);
                 this.tileData[index].attr['total'] = datapoint[datapoint.length - 1][0]
             }
 
             if (d.indexOf(".successes") > -1) {
                 let datapoint = this.data[d];
-                //point.attr['successes'] = datapoint[datapoint.length - 1][0];
-                // this.tileData.push(point);
-                // this.tileData.splice(index, 1, point)
                 this.tileData[index].attr['successes'] = datapoint[datapoint.length - 1][0]
             }
 
             if (d.indexOf(".failures") > -1) {
                 let datapoint = this.data[d];
-                // point.attr['failures'] = datapoint[datapoint.length - 1][0];
-                // this.tileData.push(point);
-                // this.tileData.splice(index, 1, point)
                 this.tileData[index].attr['failures'] = datapoint[datapoint.length - 1][0]
             }
 
             if (d.indexOf(".errors") > -1) {
                 let datapoint = this.data[d];
-                // point.attr['errors'] = datapoint[datapoint.length - 1][0];
-                // this.tileData.push(point);
-                // this.tileData.splice(index, 1, point)
                 this.tileData[index].attr['errors'] = datapoint[datapoint.length - 1][0]
             }
         }, this);
