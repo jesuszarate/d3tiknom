3fd1087eb2117ceafe6baece9f01b5bffb977492
Jesus <jesus.zarate.eng@gmail.com>
2017-11-30 02:01:06 -0700

    Added more data to pop up windows and styled it a bit.

--- static/js/overview.js
+++ static/js/overview.js
@@ -8,9 +8,7 @@ class Overview {
     constructor(data) {
 
         let divTiles = d3.select("#tiles").classed("content", true);
-        // this.margin = {top: 30, right: 20, bottom: 30, left: 50};
         this.margin = {top: 100, right: 100, bottom: 200, left: 100};
-        //Gets access to the div element created for this chart and legend element from HTML
         let svgBounds = divTiles.node().getBoundingClientRect();
         this.svgWidth = svgBounds.width - this.margin.left - this.margin.right;
         this.svgHeight = this.svgWidth / 2;
@@ -28,20 +26,16 @@ class Overview {
             .attr("height", this.svgHeight)
             .attr("transform", "translate(" + this.margin.left + ",0)");
 
-        //Domain definition for global color scale
-        let domain = [-60, -50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50, 60];
+        let domain = [0, 100];
+        let range = ["#860308", "#a50f15", "#de2d26", "#fb6a4a", "#fc9272", "#fcbba1", "#c6dbef", "#9ecae1", "#6baed6",
+            "#3182bd", "#08519c", "#063e78"];
 
-        //Color range for global color scale
-        let range = ["#063e78", "#08519c", "#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#fcbba1", "#fc9272", "#fb6a4a",
-            "#de2d26", "#a50f15", "#860308"];
-
-        //ColorScale be used consistently by all the charts
         this.colorScale = d3.scaleQuantile()
             .domain(domain)
             .range(range);
 
-        //new navigationTree(data);
-        // this.dataTree = this.getData(data);
+        this.tipWindowOpen = false;
+        this.datapoints = ["successes", "failures", "total", "errors", "high", "low", "sum", "count", "avg", "min", "max"];
         this.getData(data);
     };
 
@@ -51,7 +45,14 @@ class Overview {
         Object.keys(this.data).forEach(function (d) {
             let labelArr = d.split(".");
             let label = labelArr[labelArr.length - 3];
-
+            let path = "";
+            labelArr.forEach(function (d, i) {
+                if (i >= labelArr.length - 2) {
+                    return;
+                }
+                path += d + ".";
+            });
+            path = path.substring(0, path.length - 1);
 
             let tileData = this.tileData;
             let index = 0;
@@ -64,49 +65,20 @@ class Overview {
             if (point === undefined) {
                 point = {
                     "gubbin": label,
+                    "path": path,
                     "attr": {}
                 };
                 this.tileData.push(point);
             }
 
-            if (d.indexOf(".total") > -1) {
-                let datapoint = this.data[d];
-                this.tileData[index].attr['total'] = datapoint[datapoint.length - 1][0]
-            }
+            this.datapoints.forEach(function (dataname) {
+                if (d.indexOf("." + dataname) > -1) {
+                    let datapoint = this.data[d];
+                    this.tileData[index].attr[dataname] = datapoint[datapoint.length - 1][0]
+                }
+            }, this);
 
-            if (d.indexOf(".successes") > -1) {
-                let datapoint = this.data[d];
-                this.tileData[index].attr['successes'] = datapoint[datapoint.length - 1][0]
-            }
-
-            if (d.indexOf(".failures") > -1) {
-                let datapoint = this.data[d];
-                this.tileData[index].attr['failures'] = datapoint[datapoint.length - 1][0]
-            }
-
-            if (d.indexOf(".errors") > -1) {
-                let datapoint = this.data[d];
-                this.tileData[index].attr['errors'] = datapoint[datapoint.length - 1][0]
-            }
         }, this);
-    }
-
-
-    /**
-     * Returns the class that needs to be assigned to an element.
-     *
-     * @param party an ID for the party that is being referred to.
-     */
-    chooseClass(party) {
-        if (party == "R") {
-            return "republican";
-        }
-        else if (party == "D") {
-            return "democrat";
-        }
-        else if (party == "I") {
-            return "independent";
-        }
     }
 
     /**
@@ -116,15 +88,56 @@ class Overview {
      * @return text HTML content for tool tip
      */
     tooltip_render(tooltip_data) {
-        let text = "<h2 class =" + this.chooseClass(tooltip_data.winner) + " >" + tooltip_data.state + "</h2>";
-        text += "Electoral Votes: " + tooltip_data.electoralVotes;
+        let text = "<h2 >" + tooltip_data.gubbin + "</h2>";
         text += "<ul>";
-        tooltip_data.result.forEach((row) => {
-            //text += "<li>" + row.nominee+":\t\t"+row.votecount+"("+row.percentage+"%)" + "</li>"
-            text += "<li class = " + this.chooseClass(row.party) + ">" + row.nominee + ":\t\t" + row.votecount + "(" + row.percentage + "%)" + "</li>"
+
+        // if (tooltip_data.attr.total !== undefined) {
+        //     text += "<li>" + "Total" + ":\t\t" + tooltip_data.attr.total + "</li>";
+        // }
+        // if (tooltip_data.attr.successes !== undefined) {
+        //     text += "<li>" + "Successes" + ":\t\t" + tooltip_data.attr.successes + "</li>";
+        // }
+        // if (tooltip_data.attr.failures !== undefined) {
+        //     text += "<li>" + "Failures" + ":\t\t" + tooltip_data.attr.failures + "</li>";
+        // }
+        // if (tooltip_data.attr.errors !== undefined) {
+        //     text += "<li>" + "Errors" + ":\t\t" + tooltip_data.attr.errors + "</li>";
+        // }
+        //
+        // if (tooltip_data.attr.sum !== undefined) {
+        //     text += "<li>" + "Sum" + ":\t\t" + tooltip_data.attr.sum + "</li>";
+        // }
+        //
+        // if (tooltip_data.attr.high !== undefined) {
+        //     text += "<li>" + "High" + ":\t\t" + tooltip_data.attr.high + "</li>";
+        // }
+        //
+        // if (tooltip_data.attr.low !== undefined) {
+        //     text += "<li>" + "low" + ":\t\t" + tooltip_data.attr.low + "</li>";
+        // }
+
+        this.datapoints.forEach(function (d) {
+            if (tooltip_data.attr[d] !== undefined) {
+                if(d === "successes") {
+                    text += "<li class='successful'>" + d.toUpperCase() + ":\t\t" + tooltip_data.attr[d] + "</li>";
+                }
+                else if(d === "failures") {
+                    text += "<li class='failure'>" + d.toUpperCase() + ":\t\t" + tooltip_data.attr[d] + "</li>";
+                }
+                else {
+                    text += "<li>" + d.toUpperCase() + ":\t\t" + tooltip_data.attr[d] + "</li>";
+                }
+            }
         });
+
         text += "</ul>";
 
+        if (this.tipWindowOpen) {
+            text += "<form action=\"linechart.html\" method=\"GET\">" +
+                "  <input type=\"hidden\" name=\"gubbin\" value=\"" + tooltip_data.path + "\" />" +
+                "  <input type=\"submit\" value=\"View\"/>" +
+                "</form>";
+        }
         return text;
     }
 
@@ -143,6 +156,15 @@ class Overview {
         let width = this.svgWidth / this.maxColumns;
         let height = this.svgHeight / this.maxRows;
 
+        let tipMouseOver = d3.tip().attr('class', 'd3-tip')
+            .direction('se')
+            .offset(function () {
+                return [0, 0];
+            })
+            .html((d) => {
+                return this.tooltip_render(d);
+            });
+
         this.svg.selectAll("g").remove();
 
         let reference = this;
@@ -151,15 +173,33 @@ class Overview {
 
         tile.exit().remove();
 
+        let ref = this;
         let tileEnter = tile.enter().append("g")
             .attr("transform", function (d, i) {
-                console.log("width: " + width + ", i = " + i);
-                let translate = "translate(" + (i % 10) * width + "," + Math.floor(i / 10) * height + ")";
-                console.log(translate);
-                return translate;
+                return "translate(" + (i % 10) * width + "," + Math.floor(i / 10) * height + ")";
             }).attr("class", function (d) {
                 return "tile";
+            })
+            .on("mouseover", function (d) {
+                if (!ref.tipWindowOpen) {
+                    tipMouseOver.show(d);
+                }
+            })
+            .on("mouseleave", function (d) {
+                if (!ref.tipWindowOpen) {
+                    tipMouseOver.hide(d);
+                }
+            })
+            .on("click", function (d) {
+                ref.tipWindowOpen = true;
+                tipMouseOver.show(d);
+            })
+            .on("dblclick", function (d) {
+                tipMouseOver.hide();
+                ref.tipWindowOpen = false;
             });
+
+        tileEnter.call(tipMouseOver);
 
         this.drawTile(tileEnter, this.colorScale, height, width);
         tile.merge(tileEnter);
@@ -172,20 +212,23 @@ class Overview {
             .attr("width", width)
             .style("fill", function (d) {
                 let successes = d.attr["successes"];
+                let failures = d.attr["failures"];
                 let total = d.attr["total"];
 
-                if (total === undefined){
-                    total = 0;
+                if (total === undefined) {
+                    total = 1;
                 }
-                if (successes === undefined){
+                if (successes === undefined) {
                     successes = 0;
                 }
-                return (colorScale(successes - total));
+                if (failures === undefined) {
+                    failures = 0;
+                }
+
+                return (colorScale(((successes / total) * 100)));// - (failures/total)*100) ));
             })
-            .attr("height", height)
-            .on("click", function (d) {
-                window.open("linechart.html?gubbin=" + d["gubbin"],"_self")
-            });
+            .attr("height", height);
+
 
         let label = tile.append("text");
 
@@ -196,26 +239,14 @@ class Overview {
             .attr("x", midX + 50)
             .attr("y", midY - 10)
             .style("font-size", function () {
-                // console.log((width*height)/1565);
                 return (width * height) / 1400;
             })
             .text(function (d) {
-
                 return d["gubbin"];
             })
             .attr("class", function (d) {
                 return "tilestext";
             });
-
-        // label.append("tspan")
-        //     .attr("x", midX)
-        //     .attr("y", midY + 24)
-        //     .text(function (d) {
-        //         return d["Total_EV"];
-        //     })
-        //     .attr("class", function (d) {
-        //         return reference.chooseClass(d["Party"]) + " tilestext";
-        //     });
     }
 
 }
